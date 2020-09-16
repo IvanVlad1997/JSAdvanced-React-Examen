0022daea50ea774530b104135cf459000c417963
IvanVlad1997 <ivanvlad97@yahoo.ro>
2020-09-16 16:47:32 +0300

    Add UserList UserItem

---
+++ src/components/UserList/UserList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Table} from 'react-bootstrap'
+
+function UserList() {
+    return (
+        <div className="row" className="mt-3">
+            <div className="col-12 col-lg-3">
+
+            </div>
+
+            <div className="col-12 col-lg-9 ">
+                <Table striped bordered hover >
+                    <thead>
+                        <tr>
+                        <th>Nume</th>
+                        <th>Prenume</th>
+                        <th>Meserie</th>
+                        <th>Salariu</th>
+                        <th>Data</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+         
+                    </tbody>
+                </Table>
+            </div>            
+        </div>
+
+        
+    )
+}
+
+export default UserList
--- src/pages/People/People.jsx
+++ src/pages/People/People.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import UserList from '../../components/UserList/UserList'
 
 
 const People = () => {
@@ -8,6 +9,7 @@ const People = () => {
             <div className="row">
                 <div className="col-12 ">
                     <Link to ="/" className="btn btn-primary">Home</Link>
+                    <UserList />
                 </div>
             </div>
        </div>
